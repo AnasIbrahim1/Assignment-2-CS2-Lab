4595aa1bd529e9544ec62d6eaca3975daeec71d6
AnasIbrahim1 <anas2@aucegypt.edu>
2021-03-15 00:29:17 +0200

    Almost finished the project, changed some algorithms and naming, added some comments, ...

--- hangman1.cpp
+++ hangman1.cpp
@@ -1,10 +1,5 @@
 #include "hangman1.h"
 #include "ui_hangman1.h"
-#include<fstream>
-#include<iostream>
-#include<string>
-using namespace std;
-int hangman1::count=0;
 
 hangman1::hangman1(QWidget *parent)
     : QMainWindow(parent)
@@ -12,344 +7,333 @@ hangman1::hangman1(QWidget *parent)
 {
     ui->setupUi(this);
     scene = new QGraphicsScene(this);
-       ui->view->setScene(scene);
-       int number=0;
-       KnowinTheSize(number);
-       string* array=FillingTheArray(number);
-       word= knowinTheWord(array, number);
-    for (int i = 0; i < 26; i++)
-        CheckLetter[i] = 1;
-    for(int i = 0; i < word.length(); i++)
-        displayedWord += "_";
+    ui->view->setScene(scene);
+    getWord(); // initializes the word from the file
+    int wordLeng = word.length();
+    for(int i = 0; i < wordLeng; i++)
+        displayedWord += "_"; // initially all the displayed characters in the word are spaces
     ui->label->setText(displayedWord);
-    s_word = word;
-    ui->label_A->setVisible(0);
-    ui->label_B->setVisible(0);
-    ui->label_C->setVisible(0);
-    ui->label_D->setVisible(0);
-    ui->label_E->setVisible(0);
-    ui->label_F->setVisible(0);
-    ui->label_G->setVisible(0);
-    ui->label_H->setVisible(0);
-    ui->label_I->setVisible(0);
-    ui->label_J->setVisible(0);
-    ui->label_K->setVisible(0);
-    ui->label_L->setVisible(0);
-    ui->label_M->setVisible(0);
-    ui->label_N->setVisible(0);
-    ui->label_O->setVisible(0);
-    ui->label_P->setVisible(0);
-    ui->label_Q->setVisible(0);
-    ui->label_R->setVisible(0);
-    ui->label_S->setVisible(0);
-    ui->label_T->setVisible(0);
-    ui->label_U->setVisible(0);
-    ui->label_V->setVisible(0);
-    ui->label_W->setVisible(0);
-    ui->label_X->setVisible(0);
-    ui->label_Y->setVisible(0);
-    ui->label_Z->setVisible(0);
-
-
+    spaceCount = wordLeng;
+    labelsVisibility(0); // all labels above the buttons are not visible initially
 }
 
-hangman1::~hangman1()
-{
+hangman1::~hangman1() {
     delete ui;
 }
 
-
-void hangman1::on_A_clicked()
-{
-    checkLetter(ui->A->text());
-    ui->A->setVisible(0);
+// action when clicking the buttons
+void hangman1::on_A_clicked() {
+    checkLetter(ui->A->text()); // we search and check the letter with its text as the input
+    ui->A->setVisible(0); // right or wrong, the button becomes disabled (i.e. you can't click it)
+    // we do this by making the button disappear, and making the corresponding label appear
     ui->label_A->setVisible(1);
-    // if return false, then call animation part
 }
 
-void hangman1::on_B_clicked()
-{
+void hangman1::on_B_clicked() {
     checkLetter(ui->B->text());
     ui->B->setVisible(0);
     ui->label_B->setVisible(1);
 }
 
-void hangman1::on_C_clicked()
-{
+void hangman1::on_C_clicked() {
     checkLetter(ui->C->text());
     ui->C->setVisible(0);
     ui->label_C->setVisible(1);
 }
 
-void hangman1::on_D_clicked()
-{
+void hangman1::on_D_clicked() {
     checkLetter(ui->D->text());
     ui->D->setVisible(0);
     ui->label_D->setVisible(1);
 }
 
-void hangman1::on_E_clicked()
-{
+void hangman1::on_E_clicked() {
     checkLetter(ui->E->text());
     ui->E->setVisible(0);
     ui->label_E->setVisible(1);
 }
 
-void hangman1::on_F_clicked()
-{
+void hangman1::on_F_clicked() {
     checkLetter(ui->F->text());
     ui->F->setVisible(0);
     ui->label_F->setVisible(1);
 }
 
-void hangman1::on_G_clicked()
-{
+void hangman1::on_G_clicked() {
     checkLetter(ui->G->text());
     ui->G->setVisible(0);
     ui->label_G->setVisible(1);
 }
 
-void hangman1::on_H_clicked()
-{
+void hangman1::on_H_clicked() {
     checkLetter(ui->H->text());
     ui->H->setVisible(0);
     ui->label_H->setVisible(1);
 }
 
-void hangman1::on_I_clicked()
-{
+void hangman1::on_I_clicked() {
     checkLetter(ui->I->text());
     ui->I->setVisible(0);
     ui->label_I->setVisible(1);
 }
 
-void hangman1::on_J_clicked()
-{
+void hangman1::on_J_clicked() {
     checkLetter(ui->J->text());
     ui->J->setVisible(0);
     ui->label_J->setVisible(1);
 }
 
-void hangman1::on_K_clicked()
-{
+void hangman1::on_K_clicked() {
     checkLetter(ui->K->text());
     ui->K->setVisible(0);
     ui->label_K->setVisible(1);
 }
 
-void hangman1::on_L_clicked()
-{
+void hangman1::on_L_clicked() {
     checkLetter(ui->L->text());
     ui->L->setVisible(0);
     ui->label_L->setVisible(1);
 }
 
-void hangman1::on_M_clicked()
-{
+void hangman1::on_M_clicked() {
     checkLetter(ui->M->text());
     ui->M->setVisible(0);
     ui->label_M->setVisible(1);
 }
 
-void hangman1::on_N_clicked()
-{
+void hangman1::on_N_clicked() {
     checkLetter(ui->N->text());
     ui->N->setVisible(0);
     ui->label_N->setVisible(1);
 }
 
-void hangman1::on_O_clicked()
-{
+void hangman1::on_O_clicked() {
     checkLetter(ui->O->text());
     ui->O->setVisible(0);
     ui->label_O->setVisible(1);
 }
 
-void hangman1::on_P_clicked()
-{
+void hangman1::on_P_clicked() {
     checkLetter(ui->P->text());
     ui->P->setVisible(0);
     ui->label_P->setVisible(1);
 }
 
-void hangman1::on_Q_clicked()
-{
+void hangman1::on_Q_clicked() {
     checkLetter(ui->Q->text());
     ui->Q->setVisible(0);
     ui->label_Q->setVisible(1);
 }
 
-void hangman1::on_R_clicked()
-{
+void hangman1::on_R_clicked() {
     checkLetter(ui->R->text());
     ui->R->setVisible(0);
     ui->label_R->setVisible(1);
 }
 
-void hangman1::on_S_clicked()
-{
+void hangman1::on_S_clicked() {
     checkLetter(ui->S->text());
     ui->S->setVisible(0);
     ui->label_S->setVisible(1);
 }
 
-void hangman1::on_T_clicked()
-{
+void hangman1::on_T_clicked() {
     checkLetter(ui->T->text());
     ui->T->setVisible(0);
     ui->label_T->setVisible(1);
 }
 
-void hangman1::on_U_clicked()
-{
+void hangman1::on_U_clicked() {
     checkLetter(ui->U->text());
     ui->U->setVisible(0);
     ui->label_U->setVisible(1);
 }
 
-void hangman1::on_V_clicked()
-{
+void hangman1::on_V_clicked() {
     checkLetter(ui->V->text());
     ui->V->setVisible(0);
     ui->label_V->setVisible(1);
 }
 
-void hangman1::on_W_clicked()
-{
+void hangman1::on_W_clicked() {
     checkLetter(ui->W->text());
     ui->W->setVisible(0);
     ui->label_W->setVisible(1);
 }
 
-void hangman1::on_X_clicked()
-{
+void hangman1::on_X_clicked() {
     checkLetter(ui->X->text());
     ui->X->setVisible(0);
     ui->label_X->setVisible(1);
 }
 
-void hangman1::on_Y_clicked()
-{
+void hangman1::on_Y_clicked() {
     checkLetter(ui->Y->text());
     ui->Y->setVisible(0);
     ui->label_Y->setVisible(1);
 }
 
-void hangman1::on_Z_clicked()
-{
+void hangman1::on_Z_clicked() {
     checkLetter(ui->Z->text());
     ui->Z->setVisible(0);
     ui->label_Z->setVisible(1);
 }
 
-void hangman1::checkLetter(QString letter)
-{
-    std::string s_letter = letter.toStdString();
-    char letter1 = s_letter[0];
-    char letter2;
-    int tempo;
-    bool flag = false;
-    if (CheckLetter[letter1 - 'A']) {
-        for (int i = 0; i < word.length(); i++)
-        {
-            letter2 = s_word[i];
-            if (letter2 >= 'a' && letter2 <= 'z') {
-                tempo = letter2;
-                tempo += 'A';
-                tempo -= 'a';
-                letter2 = tempo;
-            }
-            if(letter1 == letter2)
-            {
-                displayedWord[i] = word[i];
-                flag = true;
-            }
+void hangman1::checkLetter(const QString& letter) {
+    string s_letter = letter.toStdString(); // converting the letter into a string for interacting with characters
+    char letter1 = s_letter[0]; // we take the first letter since we only worry about one letter
+    char letter2; // the letter that we get check letter1 with
+    int tempo; // for checking uppercases and lowercases
+    bool found = false; // for checking if a letter is found
+    int length = word.length();
+    for (int i = 0; i < length && spaceCount; i++) { // if the spaceCount is 0, we break out of the for loop
+       letter2 = word[i];
+       if (letter2 >= 'a' && letter2 <= 'z') { // if it's lowercase, make it uppercase
+            tempo = letter2;
+            tempo += 'A';
+            tempo -= 'a';
+            letter2 = tempo;
         }
-        CheckLetter[letter1 - 'A'] = 0;
-        ui->label->setText(displayedWord);
-        if (!flag)
-        {
-            shape();
-            inc();
+        if(letter1 == letter2) {
+            displayedWord[i] = word[i]; // displaying the letter that was found on the screen
+            spaceCount--;
+            found = true;
         }
     }
+    if (!spaceCount) winning(); // if there are no spaces, then the user/player wins
+    ui->label->setText(displayedWord); // display after the change
+    if (!found) wrongLetter();
 }
-void hangman1::shape()
-{
+
+void hangman1::buttonsVisibility(const bool& b) {
+    ui->A->setVisible(b);
+    ui->B->setVisible(b);
+    ui->C->setVisible(b);
+    ui->D->setVisible(b);
+    ui->E->setVisible(b);
+    ui->F->setVisible(b);
+    ui->G->setVisible(b);
+    ui->H->setVisible(b);
+    ui->I->setVisible(b);
+    ui->J->setVisible(b);
+    ui->K->setVisible(b);
+    ui->L->setVisible(b);
+    ui->M->setVisible(b);
+    ui->N->setVisible(b);
+    ui->O->setVisible(b);
+    ui->P->setVisible(b);
+    ui->Q->setVisible(b);
+    ui->R->setVisible(b);
+    ui->S->setVisible(b);
+    ui->T->setVisible(b);
+    ui->U->setVisible(b);
+    ui->V->setVisible(b);
+    ui->W->setVisible(b);
+    ui->X->setVisible(b);
+    ui->Y->setVisible(b);
+    ui->Z->setVisible(b);
+}
+
+void hangman1::labelsVisibility(const bool& b) {
+    ui->label_A->setVisible(b);
+    ui->label_B->setVisible(b);
+    ui->label_C->setVisible(b);
+    ui->label_D->setVisible(b);
+    ui->label_E->setVisible(b);
+    ui->label_F->setVisible(b);
+    ui->label_G->setVisible(b);
+    ui->label_H->setVisible(b);
+    ui->label_I->setVisible(b);
+    ui->label_J->setVisible(b);
+    ui->label_K->setVisible(b);
+    ui->label_L->setVisible(b);
+    ui->label_M->setVisible(b);
+    ui->label_N->setVisible(b);
+    ui->label_O->setVisible(b);
+    ui->label_P->setVisible(b);
+    ui->label_Q->setVisible(b);
+    ui->label_R->setVisible(b);
+    ui->label_S->setVisible(b);
+    ui->label_T->setVisible(b);
+    ui->label_U->setVisible(b);
+    ui->label_V->setVisible(b);
+    ui->label_W->setVisible(b);
+    ui->label_X->setVisible(b);
+    ui->label_Y->setVisible(b);
+    ui->label_Z->setVisible(b);
+}
+
+void hangman1::close() {
+    buttonsVisibility(0);
+    labelsVisibility(1);
+}
+
+void hangman1::losing() {
+    QColor color = "red";
+    ui->status->setText("<font color='red'>You lost!</font>");
+    close();
+}
+
+void hangman1::winning() {
+    QColor color = "green";
+    ui->status->setText("<font color='green'>You won!</font>");
+    close();
+}
+
+void hangman1::drawShape() {
     QBrush RedBrush(Qt::red);
     QPen Blackpen(Qt::black);
     Blackpen.setWidth(6);
-    switch (count)
-    {
-    case 0:
-            ellipse = scene->addEllipse(-50, -50, 100, 100, Blackpen, RedBrush);
-        break;
+    switch (mistakesNum) {
     case 1:
-                Line = scene->addLine(0, 55, 0, 250, Blackpen);
-    break;
+        ellipse = scene->addEllipse(-50, -50, 100, 100, Blackpen, RedBrush);
+        break;
     case 2:
-                Line = scene->addLine(0, 100, 100, 150, Blackpen);
-                break;
+        Line = scene->addLine(0, 55, 0, 250, Blackpen);
+        break;
     case 3:
-    Line = scene->addLine(0, 100, -100, 150, Blackpen);
-    break;
+        Line = scene->addLine(0, 100, 100, 150, Blackpen);
+        break;
     case 4:
-    Line = scene->addLine(0, 250,-100, 300, Blackpen);
+        Line = scene->addLine(0, 100, -100, 150, Blackpen);
         break;
     case 5:
-    Line = scene->addLine(0, 250, 100, 300, Blackpen);
+        Line = scene->addLine(0, 250,-100, 300, Blackpen);
         break;
     case 6:
-    Line = scene->addLine(-200, -100, -200, 300, Blackpen);
+        Line = scene->addLine(0, 250, 100, 300, Blackpen);
         break;
     case 7:
-    Line = scene->addLine(-200, -100, 0, -100, Blackpen);
+        Line = scene->addLine(-200, -100, -200, 300, Blackpen);
         break;
     case 8:
-    Line = scene->addLine(0, -100, 0, -50, Blackpen);
-    exit(1);
+        Line = scene->addLine(-200, -100, 0, -100, Blackpen);
         break;
-}
-}
-void hangman1::inc()
-{
-    count++;
-}
-int hangman1:: getcount()
-{
-    return count;
-}
-void hangman1::KnowinTheSize(int&number)
-{
-    ifstream object;
-    string line;
-    object.open("Words.txt");
-    while (getline(object,line))
-    {
-        number++;
+    case 9:
+        Line = scene->addLine(0, -100, 0, -50, Blackpen);
+        losing();
+        break;
     }
-    object.close();
 }
-string* hangman1::FillingTheArray(int number)
-{
-    string line;
-    string* array = new string[number];
-    ifstream object;
-    object.open("Words.txt");
+
+void hangman1::wrongLetter() {
+    mistakesNum++;
+    drawShape();
+}
+
+void hangman1::getWord() {
+    srand(time(0));
+    int number;
+    ifstream wordsFile;
+    wordsFile.open("C:/Users/AUC/Desktop/Spring 2021/CS2 Lab/QT Projects/Assignment 2 Work/Words.txt");
+    // put the directory of the file of input that you want for input
+    wordsFile >> number;
+    // note that the format of the file includes having a number of words that you are going to have for input
+    // and then the words that the program chooses randomly from
+    int randomNumber = rand() % number;
     for (int i = 0; i < number; i++)
     {
-
-        if (getline(object, line))
-        {
-            array[i] = line;
-        }
+        wordsFile >> word;
+        if (i == randomNumber) break;
     }
-    object.close();
-    return array;
 }
-string hangman1::knowinTheWord(string *array, int number)
-{
-    srand(time(0));
-        return array[rand()%number+0];
-}
-
--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include "hangman1.h"
-
 #include <QApplication>
-#include<fstream>
-#include<iostream>
-#include<string>
-using namespace std;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
